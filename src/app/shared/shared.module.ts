57032d642350eede8a4c15de559fd3aa5addcc4f
Jonash Marcelino <thewwwnash@gmail.com>
2024-05-22 21:49:57 +0800

    Edit page not found.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,10 +6,11 @@ import { TranslateModule } from '@ngx-translate/core';
 import { PageNotFoundComponent } from './components/';
 import { WebviewDirective } from './directives/';
 import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @NgModule({
   declarations: [PageNotFoundComponent, WebviewDirective],
-  imports: [CommonModule, TranslateModule, FormsModule],
-  exports: [TranslateModule, WebviewDirective, FormsModule]
+  imports: [CommonModule, TranslateModule, FormsModule, RouterLink],
+  exports: [TranslateModule, WebviewDirective, FormsModule],
 })
 export class SharedModule {}
